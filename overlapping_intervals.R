de016b9b6f46564ea3a9cf04c7d1237d297ae3df
fleoren <mfalcalad@gmail.com>
2016-02-16 12:41:59 -0600

    Important comments, creating a nice toy dataset, also cleaning up a bit the variable names.

--- overlapping_intervals.R
+++ overlapping_intervals.R
@@ -1,11 +1,20 @@
-#creando unos datitos de juguete
+#CREATING A TOY DATASET
 minimo <- runif(1)
-maximo <- runif(1)*10
-df1 <- as.data.frame(seq(minimo,maximo,by=0.1))
-names(df1)=c("x1")
+maximo <- 1+runif(1)*9
+df <- data.frame(x1=seq(minimo,maximo,by=0.1))
+df$x2 <- c(rnorm(n=floor(dim(df)[1]/2),mean=10,sd=2),  #let's make this variable induce some clusters
+           rnorm(n=ceiling(dim(df)[1]/2),mean=0,sd=1))
+plot(df)
+
+################### IMPORTANT
+#BEFORE THIS SECTION IT IS IMPORTANT FOR THE DATA FRAME THAT WILL BE USED TO BE CALLED "df"
+#ALSO I AM ASSUMING WE CAN TAKE "x1" AS A VARIABLE FROM "df" TO SUBSET
+#THIS VARIABLE HAS TO BE NUMERIC AND NON CATEGORICAL
+#I SUGGEST WORKING WITH THE TOY DATA SET FIRST AND THEN MOVING ON TO TRY OTHER ONES
+####################
 
 #----------------------------- NECESSARY PARAMETERS -----------------------------
-var_o <- df1$x1   #variable we will use to make the overlapping subsets
+var_o <- df$x1   #variable we will use to make the overlapping subsets
 n_int <- 10       #number of intervals we want
 p <- 0.2          #proportion of each interval that should overlap with the next
 
@@ -22,11 +31,16 @@ intervals <- data.frame(centers=intervals_centers)            #create a data fra
 intervals$name <- with(intervals, sprintf("[%.2f;%.2f)",min,max))
 
 #function that will split the variable according to the invervals
-#this function returns a list
-res <- lapply(split(intervals,intervals$name), function(x){
-  return(df1[var_o> x$min & var_o <= x$max,])
-})
+res <- lapply(split(intervals,intervals$name), function(x){   
+  return(df[var_o> x$min & var_o <= x$max,])     #res will be a list with each element res[i]
+})                                                #being the points on the i'th subset
 
-res
+#res
+
+#----------------------------- NEXT STEPS -----------------------------
+#ITERATE EVERY ELEMENT OF THE LIST (res[i]) AND CLUSTERIZE INSIDE
+#SEE HOW THEY ARE LABELED
+#DETERMINE WHICH CLUSTERS SHARE POINTS AND JOIN THEM (PUT A 1 ON THE ADJACENCY MATRIX BASICALLY)
+#MAGIC
 
 
